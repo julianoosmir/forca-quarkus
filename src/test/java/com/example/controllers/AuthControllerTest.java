13e9b53d058837a8b7ca3e259ce7642377ffddc1
juliano <julianoosmir@outlook.com>
2024-11-22 16:37:30 -0400

    testcontainers

---
+++ src/test/java/com/example/controllers/AuthControllerTest.java
@@ -0,0 +1,27 @@
+package com.example.controllers;
+
+import com.example.DatabaseDockerInicializer;
+import com.example.dto.JwtRequest;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+
+@QuarkusTest
+@QuarkusTestResource(DatabaseDockerInicializer.class)
+public class AuthControllerTest {
+
+    @Test
+    public void login() {
+        JwtRequest jwtRequest = new JwtRequest("julianoosmir", "$123456");
+
+        given()
+                .when()
+                .contentType("application/json")
+                .body(jwtRequest)
+                .post("/auth")
+                .then()
+                .log().all()
+                .statusCode(200);
+    }
+}

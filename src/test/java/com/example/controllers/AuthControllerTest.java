38bf85b1a7a35298d2a002047d7932fee2f926fb
juliano <julianoosmir@outlook.com>
2024-11-26 11:05:18 -0400

    WithTestResource

--- src/test/java/com/example/controllers/AuthControllerTest.java
+++ src/test/java/com/example/controllers/AuthControllerTest.java
@@ -2,13 +2,13 @@ package com.example.controllers;
 
 import com.example.DatabaseDockerInicializer;
 import com.example.dto.JwtRequest;
-import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.common.WithTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 
 @QuarkusTest
-@QuarkusTestResource(DatabaseDockerInicializer.class)
+@WithTestResource(DatabaseDockerInicializer.class)
 public class AuthControllerTest {
 
     @Test
